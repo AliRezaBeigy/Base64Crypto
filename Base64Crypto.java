38db4635308d6bc714fa40036ec81373aafee602
BijanEisapour <40728713+BijanEisapour@users.noreply.github.com>
2018-12-26 14:04:07 +0330

    Add files via upload

---
+++ Base64Crypto.java
@@ -0,0 +1,40 @@
+import java.util.Base64;
+
+public class Base64Crypto
+{
+	static final String SEPARATOR = "----------------------------------------";
+	static String[] tests = new String[] { "Hello" , "This is a test" , "I want to encode me then decode me :D" };
+	
+	public static void main ( String[] args )
+	{
+		for ( String test : tests )
+		{
+			String encoded = encrypt ( test );
+			String decrypted = decrypt ( encoded );
+			System.out.printf ( "%s%n%s -> %s%n%s -> %s%n" , SEPARATOR , test , encoded , encoded , decrypted );
+		}
+	}
+	
+	static String encrypt ( String plainText )
+	{
+		return Base64.getEncoder ().encodeToString ( plainText.getBytes () );
+	}
+	
+	static String decrypt ( String base64 )
+	{
+		return new String ( Base64.getDecoder ().decode ( base64 ) );
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//                                             Output                                                   //
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//      Hello -> SGVsbG8=                                                                               //
+	//      SGVsbG8= -> Hello                                                                               //
+	//      ----------------------------------------                                                        //
+	//      This is a test -> VGhpcyBpcyBhIHRlc3Q=                                                          //
+	//      VGhpcyBpcyBhIHRlc3Q= -> This is a test                                                          //
+	//      ----------------------------------------                                                        //
+	//      I want to encode me then decode me :D -> SSB3YW50IHRvIGVuY29kZSBtZSB0aGVuIGRlY29kZSBtZSA6RA==   //
+	//      SSB3YW50IHRvIGVuY29kZSBtZSB0aGVuIGRlY29kZSBtZSA6RA== -> I want to encode me then decode me :D   //
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
