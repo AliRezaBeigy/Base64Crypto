21196c435e26909af201ea243fdf72bd250e6feb
mmahdisonic <m.mahdi_sonic@yahoo.com>
2018-12-28 17:35:28 +0330

    Update Base64Crypto.java

--- Base64Crypto.java
+++ Base64Crypto.java
@@ -1,4 +1,6 @@
-package base64crypto;
+//package base64crypto;
+
+import java.util.Base64;
 
 public class Base64Crypto {
 
@@ -14,14 +16,13 @@ public class Base64Crypto {
     }
 
     static String encrypt(String plainText) {
-        String result = plainText;
-        // Compelet it
+        String result = Base64.getEncoder().encodeToString(plainText.getBytes());
         return result;
     }
 
     static String decrypt(String base64) {
-        String result = base64;
-        // Compelet it
+        byte[] decoded = Base64.getDecoder().decode(base64);
+        String result = new String(decoded);
         return result;
     }
     
